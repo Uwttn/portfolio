2c3a3bf42113fa6755d11443e6d44fc51bc8d160
jm <133266199+Uwttn@users.noreply.github.com>
2024-10-16 07:40:00 -0400

    Revamping portfolio again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,4 @@
-// // Creates Navigation Bar on all pages
-
-// const nav = document.getElementsByTagName("nav");
-
-// nav[0].innerHTML = `
-// <ul>
-// <a id="home" class="navTab"><li class="navLi">Home</li></a>
-// <a id="team" class="navTab"><li class="navLi">Team Projects</li></a>
-// </ul>
-// `;
-
-// // Maps the links on the Navigation Bar from Index
-
-// function setLinks() {
-//   const homePage = window.location.pathname;
-
-//   let pageMapping;
-
-//   if (homePage.includes("index.html")) {
-//     pageMapping = {
-//       home: "./index.html",
-//       team: "./team.html",
-//     };
-//   } else {
-//     pageMapping = {
-//       home: "./index.html",
-//       team: "./team.html",
-//     };
-//   }
-//   for (const id in pageMapping) {
-//     const linkElement = document.getElementById(id);
-//     linkElement.href = pageMapping[id];
-//   }
-// }
-
-setLinks();
-
 // function that flips the card onclick
 function toggleFlip(card) {
-  card.classList.toggle("flipped");
-}
+    card.classList.toggle("flipped");
+  }
